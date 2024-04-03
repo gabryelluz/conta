0a8222999a34362728c1f051dec37c1b5364529d
gabryelluz <130998782+gabryelluz@users.noreply.github.com>
2024-04-03 08:40:20 -0300

    ...

--- cont.js
+++ cont.js
@@ -3,4 +3,8 @@ const botões = document.querySelectorAll('.Objetivos');
 
 for (let i = 0; i <botões.lenght; i++){
 console.log(i)
+botões[i].onclick = function(){
+    botões[i].classList.add('ativo');
+}
+// botões[i].classList.remove ('ativo');
 }
