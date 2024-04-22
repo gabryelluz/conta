58d4bd9aff182c79fd74be0328d34c4acb8acc02
gabryelluz <130998782+gabryelluz@users.noreply.github.com>
2024-04-22 12:11:01 -0300

    .

--- cont.js
+++ cont.js
@@ -15,7 +15,16 @@ for (let i = 0; i <botões.length; i++){
 
 const contadores = document.querySelectorAll('.contador');
 const TempoObjetivo1 = new Date ('2024-04-16T00:00:00');
+const TempoObjetivo2 = new Date ('2024-04-16T00:00:00');
+const TempoObjetivo3 = new Date ('2024-04-16T00:00:00');
+const TempoObjetivo4 = new Date ('2024-04-16T00:00:00');
 
+let Tempos = {TempoObjetivo1, TempoObjetivo2, TempoObjetivo3, TempoObjetivo4};
+for(i = 0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(Tempos[i]);
+}
+
+function calculaTempo (TempoObjetivo){
 let TempoAtual = new Date ();
 let tempo = TempoObjetivo1 - TempoAtual;
 let segundos = Math.floor(TempoFinal/1000);
@@ -23,4 +32,9 @@ let minutos = Math.floor (segundos/60);
 let horas = Math.floor (minutos/60);
 let dias = Math.floor (horas/24);
 
-contadores[0].textContent = dias + "dias" + horas + "horas" + minutos +"minutos" + segundos + "segundos";
+segundos %=60;
+minutos %=60;
+horas %=24;
+
+ return dias + "dias" + horas + "horas" + minutos +"minutos" + segundos + "segundos";
+}
