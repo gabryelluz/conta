715540a88dac0c11df7e2085f88dcfdbea715b43
gabryelluz <130998782+gabryelluz@users.noreply.github.com>
2024-04-22 11:57:47 -0300

    aa

--- cont.js
+++ cont.js
@@ -12,3 +12,15 @@ for (let i = 0; i <botões.length; i++){
     textos[i].classlist.add('ativa');
 }
 }
+
+const contadores = document.querySelectorAll('.contador');
+const TempoObjetivo1 = new Date ('2024-04-16T00:00:00');
+
+let TempoAtual = new Date ();
+let tempo = TempoObjetivo1 - TempoAtual;
+let segundos = Math.floor(TempoFinal/1000);
+let minutos = Math.floor (segundos/60);
+let horas = Math.floor (minutos/60);
+let dias = Math.floor (horas/24);
+
+contadores[0].textContent = dias + "dias" + horas + "horas" + minutos +"minutos" + segundos + "segundos";
